df72e04fcf1d4af7b7de947f90e14dbe39f325dd
timf1018 <timmy@timmyfisher.com>
2019-06-05 13:43:17 -0400

    Fixed index method bug when creating new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
     if @article.save
       #do something
       flash[:notice] = "Article was successfully created"
-      redirect_to articles_path(@article)
+      redirect_to article_path(@article)
     else
       render 'new'
     end
