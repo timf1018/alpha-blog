1137827270ce43391872271896aa0a242fc6a5ac
timf1018 <timmy@timmyfisher.com>
2019-06-10 14:49:55 -0400

    swtiched order of jquery requirements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 
-
-//= require jquery_ujs
 //= require jquery
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require rails-ujs
 //= require activestorage
